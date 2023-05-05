ecac969943cdd242d97561a843d14d071a63e653
FinnTheHero <127041443+FinnTheHero@users.noreply.github.com>
2023-05-06 03:47:32 +0400

    Updating frontend
    Fetching items from front end using react

---
+++ models/itemModel.js
@@ -0,0 +1,23 @@
+const { Sequelize, DataTypes } = require('sequelize')
+const sequelize = require('../database.js')
+
+// Setup Item Model
+const Item = sequelize.define('Item',{
+    name:{
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.SMALLINT,
+        allowNull: false
+    },
+    location: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+},{
+    timestamps: false,
+    tableName: 'Items'
+})
+
+module.exports = Item
